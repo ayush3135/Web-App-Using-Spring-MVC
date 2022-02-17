9fe893561cb164f0d89cc104b5b2395fd8577f2e
ayush@3135 <ayushverma0008@gmail.com>
2022-02-17 14:50:04 +0530

    adding all files to repo

---
+++ src/main/java/com/telusko/demo/HomeController.java
@@ -0,0 +1,29 @@
+package com.telusko.demo;
+
+//import javax.servlet.http.HttpServletRequest;
+//
+//import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController 
+{
+       @RequestMapping("home")
+       
+       public ModelAndView home(Alien alien)
+       {
+       	   
+	   ModelAndView mv = new ModelAndView();
+	   mv.addObject("obj", alien);
+	   mv.setViewName("home");
+	   
+	   return mv;
+	
+       }	
+	
+}
